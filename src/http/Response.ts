376d64977036d5807f4dad400404ec96a7d98710
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-19 23:58:14 +0530

    init core methods

--- src/http/Response.ts
+++ src/http/Response.ts
@@ -2,9 +2,31 @@ import { ServerResponse } from "http";
 
 export class Response extends ServerResponse {
 
-    public sendHelloWorld() {
-        this.statusCode = 200;
-        this.end("Hello world");
+    /**
+     * The content to be send as response.
+     * 
+     * @var any
+     */
+    protected _content: string = "";
+
+    /**
+     * Sends the response to the client and closes the stream. Before
+     * sending the content, we will set the appropriate Content-type and
+     * Content-length header.
+     */
+    public send() {
+
+        this.setHeader('Content-Length', this._content.length);
+
+        this.end(this._content);
     }
 
+    /**
+     * Sets the response content/body
+     * 
+     * @param content 
+     */
+    public setContent(content: any) {
+        this._content = content;
+    }
 }
