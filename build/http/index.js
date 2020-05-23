aeb9af1445369701f2bc4a556f627613f9b7b885
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-23 21:19:14 +0530

    new build after file rename

--- build/http/index.js
+++ build/http/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Request_1 = require("./Request");
-exports.Request = Request_1.Request;
-var Response_1 = require("./Response");
-exports.Response = Response_1.Response;
-var Cookies_1 = require("./Cookies");
-exports.Cookies = Cookies_1.Cookies;
+var request_1 = require("./request");
+exports.Request = request_1.Request;
+var response_1 = require("./response");
+exports.Response = response_1.Response;
+var cookies_1 = require("./cookies");
+exports.Cookies = cookies_1.Cookies;
