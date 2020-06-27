0724eb83f55e16d6bf59abfb1894a199c71cfb05
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-28 01:11:33 +0530

    refactored

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { Application } from "./app";
-import { Container } from "./container";
-import { ServiceProvider } from "./serviceProvider";
-import { DeferredServiceProvider } from "./deferredServiceProvider";
 
 export {
-    Application,
-    Container,
-    ServiceProvider,
-    DeferredServiceProvider
+    Application
 }
