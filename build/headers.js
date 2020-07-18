9e38aa3b28a665b7037d6fceaf6ddcddd5ef4170
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-18 23:34:37 +0530

    headers init

---
+++ build/headers.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Headers = /** @class */ (function () {
+    function Headers() {
+    }
+    return Headers;
+}());
+exports.Headers = Headers;
