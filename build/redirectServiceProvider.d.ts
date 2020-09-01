db0a1c2dddd9ac2074258ce86f003567e5081ff5
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-02 00:13:12 +0530

    remove build directory

--- build/redirectServiceProvider.d.ts
+++
@@ -1,8 +0,0 @@
-import { DeferredServiceProvider } from "@rheas/services";
-export declare class RedirectServiceProvider extends DeferredServiceProvider {
-    /**
-     * As this is a deferred service, redirector will be registerd only
-     * when the service is requested.
-     */
-    register(): void;
-}
