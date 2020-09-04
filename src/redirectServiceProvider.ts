82151efddd1745ce7d548af9d6430102be39b805
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-04 16:52:39 +0530

    used service resolver instead of register

--- src/redirectServiceProvider.ts
+++ src/redirectServiceProvider.ts
@@ -3,18 +3,20 @@ import { IRequest } from '@rheas/contracts';
 import { IApp } from '@rheas/contracts/core';
 import { IUrlGenerator } from '@rheas/contracts/routes';
 import { DeferredServiceProvider } from '@rheas/services';
+import { InstanceHandler } from '@rheas/contracts/container';
 
 export class RedirectServiceProvider extends DeferredServiceProvider {
     /**
-     * As this is a deferred service, redirector will be registerd only
-     * when the service is requested.
+     * Returns the redirector service resolver.
+     *
+     * @returns
      */
-    public register() {
-        this.container.singleton(this.name, (request) => {
+    public serviceResolver(): InstanceHandler {
+        return (request) => {
             const app: IApp = request.get('app');
             const urlGenerator: IUrlGenerator = app.get('url');
 
             return new Redirector(urlGenerator, request as IRequest, request.get('response'));
-        });
+        };
     }
 }
